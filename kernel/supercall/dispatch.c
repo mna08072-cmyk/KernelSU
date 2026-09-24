8769f66c868260292ec1ab754f8b96848b489a2b
mna08072-cmyk <289489057+mna08072-cmyk@users.noreply.github.com>
2026-09-24 05:31:33 +0000

    kernel: include sched/signal.h for task_pgrp/task_session

--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -1,6 +1,7 @@
 #include <linux/capability.h>
 #include <linux/cred.h>
 #include <linux/slab.h>
+#include <linux/sched/signal.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/thread_info.h>
